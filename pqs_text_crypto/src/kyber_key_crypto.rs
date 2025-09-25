cd2380d424bbdf8f52ad797638cef2dc7222cc20
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2025-09-25 15:46:07 -0500

    change to kyber rs file

--- pqs_text_crypto/src/kyber_key_crypto.rs
+++ pqs_text_crypto/src/kyber_key_crypto.rs
@@ -2,7 +2,7 @@ use oqs::*;
 use std::fs;
 
 pub fn generate_kyber_keys() -> Result<()>{
-    let kem = kem::Kem::new(kem::Algorithm::MlKem512)?;
+    let kem = kem::Kem::new(kem::Algorithm::Kyber512)?;
 
     // Generate key pair
     let (public_key, secret_key) = kem.keypair()?;
@@ -12,6 +12,7 @@ pub fn generate_kyber_keys() -> Result<()>{
     fs::write("../secret_key.bin", &secret_key).unwrap();
 
     //println!("Keys written successfully!");
+    
 
     Ok(())
 }
