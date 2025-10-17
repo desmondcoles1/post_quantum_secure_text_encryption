abc9a6cfebff3fb910871e43149ebcad5e042e78
Desmond <63413811+desmondcoles1@users.noreply.github.com>
2025-10-17 09:23:47 -0500

    changed CLI from interactive to argument based
    Removed successive prompts in favor of using CLAP along with commands for key generation, encryption, and decryption

--- pqs_text_crypto/src/main.rs
+++ pqs_text_crypto/src/main.rs
@@ -4,18 +4,42 @@ mod file_decryption; //file decryption module
 
 mod error_handling;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    KeyGeneration,
+    Encrypt,
+    Decrypt,
+}
+
+fn run_app() ->std::result::Result<(), error_handling::CryptoError> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::KeyGeneration => {
+            kyber_keypair_generation::kyber_keys_prompt()?;
+        }
+        Commands::Encrypt => {
+            file_encryption::file_encryption_prompt()?;
+        }
+        Commands::Decrypt => {
+            file_decryption::file_decryption_prompt()?;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     if let Err(e) = run_app() {
         eprintln!("Operation failed: {}", e); // just prints the error type/message to the user
     }
 }
 
-fn run_app() -> std::result::Result<(), error_handling::CryptoError>{
-    
-    kyber_keypair_generation::kyber_keys_prompt()?;
-
-    file_encryption::file_encryption_prompt()?;
-
-    file_decryption::file_decryption_prompt()?;
-    Ok(())
-}
